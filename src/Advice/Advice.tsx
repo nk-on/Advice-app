2f1ada02fa334955982a07c68fc5e02f88a1ba49
nk-on <johndoe@example.com>
2024-12-05 22:38:11 +0400

    Refactor:Adding directly Api object into useState instead of individual variable

--- src/Advice/Advice.tsx
+++ src/Advice/Advice.tsx
@@ -4,16 +4,16 @@ export default function Advice(){
     interface ApiType{
         slip:{id:number,advice:string}
     }
-    const [advice,setAdvice] = useState<string>('')
+    const [ApiObj,setApiObj] = useState<{id:number,advice:string}>({id:0,advice:''})
     async function fetchData():Promise<void>{
         const data = await fetch('https://api.adviceslip.com/advice');
         const res:ApiType = await data.json();
-        setAdvice(res?.slip?.advice);
+        setApiObj(res.slip);
     }
     useEffect(()=>{
        fetchData();
     },[])
     return (
-        <h1 className='text-[32px] text-[#c5e2e9] font-extrabold'>{advice}</h1>
+        <h1 className='text-[16px] text-[#c5e2e9] font-extrabold'>{ApiObj.advice}</h1>
     )
 }
