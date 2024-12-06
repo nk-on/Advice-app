31cb5dc755097f118c12e66bbcd733b4766d4c9c
nk-on <johndoe@example.com>
2024-12-06 22:01:29 +0400

    Fix:prenting API from generateing same advice twice

--- src/Advice/Advice.tsx
+++ src/Advice/Advice.tsx
@@ -4,19 +4,28 @@ export default function Advice(){
     interface ApiType{
         slip:{id:number,advice:string}
     }
-    const [ApiObj,setApiObj] = useState<{id:number,advice:string}>({id:0,advice:''})
+    const [ApiObj,setApiObj] = useState<{id:number,advice:string}>({id:0,advice:''});
+    const [clicked,setClicked] = useState(false);
     async function fetchData():Promise<void>{
         const data = await fetch('https://api.adviceslip.com/advice');
         const res:ApiType = await data.json();
-        setApiObj(res.slip);
+        if(res.slip.id === ApiObj.id){
+            fetchData();
+        }else{
+            setApiObj(res.slip);
+        }
     }
     useEffect(()=>{
        fetchData();
-    },[])
+       console.log('i work')
+    },[clicked])
     return (
-        <div>
+        <div className="flex flex-col justify-center items-center gap-[10px]">
             <p className="text-[#53fea8]">Advice #{ApiObj.id}</p>
             <h1 className='text-[16px] text-[#c5e2e9] font-extrabold'>{ApiObj.advice}</h1>
+            <img src="src/assets/icon-dice.svg" alt="dice-icon" className="w-[30px] h-[30px] cursor-pointer" onClick={()=>{
+                setClicked(!clicked)
+            }}></img>
         </div>
     )
 }
